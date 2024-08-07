7015819b7bc9c9e806434783078858a7adae1971
George Knap <g.knap@solar-log.com>
2024-08-07 09:10:52 +0200

    proved eslint broken for module boundaries

--- apps/ng18.1/src/app/app.component.ts
+++ apps/ng18.1/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooComponent } from '@nx19.5.6/foo';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, FooComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
